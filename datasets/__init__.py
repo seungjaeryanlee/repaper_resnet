244cf5d1687c3a459d422fe842303591a36ec346
Ryan Lee <seungjaeryanlee@gmail.com>
2023-04-30 21:49:15 -0400

    Remove long-tail datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,8 +3,6 @@ import logging
 
 from torchvision.datasets import CIFAR10, CIFAR100
 
-from .long_tail_cifar import CIFAR10LT, CIFAR100LT
-
 
 def init_dataset(config, transform, train):
     """Initialize dataset.
@@ -19,16 +17,8 @@ def init_dataset(config, transform, train):
     """
     if config.name == "CIFAR10":
         return CIFAR10(root="./data", train=train, transform=transform, download=True)
-    if config.name == "CIFAR10LT":
-        return CIFAR10LT(
-            ir=config.ir, root="./data", train=train, transform=transform, download=True
-        )
     if config.name == "CIFAR100":
         return CIFAR100(root="./data", train=train, transform=transform, download=True)
-    if config.name == "CIFAR100LT":
-        return CIFAR100LT(
-            ir=config.ir, root="./data", train=train, transform=transform, download=True
-        )
 
     logging.error(f"Could not create dataset {config.name}.")
     raise ValueError(f"Could not create dataset {config.name}.")
