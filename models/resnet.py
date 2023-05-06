50cb4a444c108a92ec5b7dae1d8e850945156235
Ryan Lee <seungjaeryanlee@gmail.com>
2023-05-06 09:57:56 -0400

    Use modules instead of functions

--- models/resnet.py
+++ models/resnet.py
@@ -71,6 +71,7 @@ class BasicBlock(nn.Module):
             planes, planes, kernel_size=3, stride=1, padding=1, bias=False
         )
         self.bn2 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
@@ -101,10 +102,13 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         """Forward pass."""
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.relu(out)
         return out
 
 
@@ -134,9 +138,11 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.pool = nn.AvgPool2d(8)
         self.linear = nn.Linear(64, num_classes)
 
         # From https://github.com/pytorch/vision/blob/0d75d9e/torchvision/models/resnet.py#L208
@@ -169,11 +175,16 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         """Forward pass."""
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = F.avg_pool2d(out, out.size()[3])
+
+        out = self.pool(out)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
+
         return out
